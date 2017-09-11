8b54ccb41cda4ca149b3ffc7254bed495e9eb9b6
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-09-11 11:54:36 +0800

    refs

--- build/CreateId-0.0.1.js
+++
@@ -1,6 +0,0 @@
-var i = 0;
-var base_id = new Date().getTime();
-module.exports = function() {
-    i++;
-    return 'creatid__id__' + base_id + i;
-}
--- build/Verification-0.0.1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function verificationPhone(phoneNumber) {
-    return /^1[3578]\d{9}$/.test(phoneNumber);
-}
-exports.verificationPhone = verificationPhone;
