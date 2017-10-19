28c8719e4ff57b3709122d3b5bfd69aef656138c
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-10-19 14:59:58 +0800

    verification email

--- ts/Verification-0.0.1.ts
+++ ts/Verification-0.0.1.ts
@@ -1,3 +1,6 @@
-export function verificationPhone(phoneNumber: string) {
+export function verificationPhone(phoneNumber: string = '') {
 	return /^1[3578]\d{9}$/.test(phoneNumber)
+}
+export function verificationEmail(email: string = ''){
+	return /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/.test(email);
 }
